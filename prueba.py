e947e0442023a717270de7362f0bb480a1635bab
Saul <espinosasaul41@gamil.com>
2026-04-04 14:51:55 -0600

    Correcciones del diagrama entidad relacion

--- prueba.py
+++ prueba.py
@@ -1,26 +1,3 @@
-#crear el cubito olap para el analisis predictivo 
-import sqlite3
+import duckdb
 
-# 1. Crear la conexión a la base de datos
-conexion = sqlite3.connect('incendios_tlaxcala.db')
-# 2. Crear el objeto cursor
-cursor = conexion.cursor()
-
-# 3. Corregir execute y foreign_keys
-cursor.execute("PRAGMA foreign_keys = ON;")
-
-def crear_dimesiones():
-    #TABLA DE HECHOS INCENDIO
-    cursor.execute(''' 
-        CREATE TABLE IF NOT EXISTS INCENDIOS (
-            id_incendio INTEGER PRIMARY KEY AUTOINCREMENT,
-            anio INTEGER,
-            clave_incendio TEXT,
-            fecha_inicio TEXT,
-            fecha_termino TEXT,
-            duracion_dias INTEGER,
-            tipo_vegetacion TEXT,
-            causa TEXT,
-            total_hectareas REAL
-        )
-    ''')
+CREATE TABLE 
